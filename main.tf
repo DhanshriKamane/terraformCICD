13455aa84602219499bc0f6d1e3abfc4d0d98582
DhanshriKamane <dhanukamane11@gmail.com>
2024-06-12 00:00:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "test" {
+resource "aws_instance" "dev" {
     ami = "ami-0e1d06225679bc1c5"
     instance_type = "t2.nano"
     tags = {
